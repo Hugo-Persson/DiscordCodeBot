a2e0a3bd95be63ea10b64870f5e33ec1c093fcbb
Hugo Persson <hugopersso7@gmail.com>
2021-01-16 21:26:24 +0100

    Removed run one line

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,6 @@ client.on("message", (msg) => {
             case "$run":
                 run(msg, chunks);
                 break;
-            case "$runLine":
-                runLine(msg, chunks);
-                break;
             case "$library":
                 library(msg, chunks);
                 break;
@@ -35,34 +32,29 @@ function help(msg: Discord.Message, chunks: Array<string>) {
     msg.reply(`
         You have following commands available 
         * $help - Will display all available commands
-        * $runLine {code} - Will run a line of code without formatting
-        * $run {code} - Will run formatted code inside code block
+        * $run {code} - Will run the code, you can run code inside code blocks
         * $saveScript {code} - Will start saving a script, you will be asked for name, you have following flags available -p --public -sg --severGlobal
         * $library will display available scripts you have stored
         * $serverScripts
         * $subscribe {id}
     `);
 }
-function runLine(msg: Discord.Message, chunks: Array<string>) {
-    if (chunks.length) {
-        const code = chunks[0];
-        executeCodeContainer(code, msg);
-    } else {
-        msg.reply("No arguments sent, please use syntax $runLine {code}");
-    }
-}
 
 function run(msg: Discord.Message, chunks: Array<string>) {
-    const concattedString: string = chunks.join(" ");
-    const codeStartIndex = concattedString.search("```");
-    let filteredCode = concattedString.substr(
-        codeStartIndex,
-        concattedString.lastIndexOf("```") - 1
-    );
+    executeCodeContainer(parseCodeInput(chunks.join(" ")), msg);
+}
 
-    filteredCode = filteredCode.substr(filteredCode.search("\n"));
+function parseCodeInput(code: string): string {
+    let codeStartIndex = code.search("```");
+    if (codeStartIndex === -1) codeStartIndex = 0;
+    let codeEnd = code.lastIndexOf("```");
+    if (codeEnd === -1) codeEnd = undefined;
+    else codeEnd -= 1;
+    let filteredCode = code.substr(codeStartIndex, codeEnd);
+    let lineBreak = filteredCode.search("\n");
 
-    executeCodeContainer(filteredCode, msg);
+    if (lineBreak !== -1) filteredCode = filteredCode.substr(lineBreak);
+    return filteredCode;
 }
 function library(msg: Discord.Message, chunks: Array<string>) {}
 function executeCodeContainer(code: string, msg: Discord.Message) {
@@ -75,6 +67,7 @@ function executeCodeContainer(code: string, msg: Discord.Message) {
         ${code}
     })();
     `;
+    console.log("CODE", code);
     try {
         const result = safeEval(code, { console: { log: fakeConsoleLog } });
         msg.reply("\n" + returnMessages.join("\n"));
